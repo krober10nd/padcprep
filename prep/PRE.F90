ff3d0b6f1fafb085704e7f5f7d0a9885cb6d6056
krober10 <keithrbt0@gmail.com>
2016-11-13 21:58:31 -0500

    Debugged the graph bisection. Splits the graph into NE/NPROC graph files.

--- prep/PRE.F90
+++ prep/PRE.F90
@@ -9,10 +9,10 @@ CHARACTER(LEN=*), PARAMETER ::FILEEND  = ".14"
 CHARACTER(LEN=20) :: FILENAME
 INTEGER             :: NE, NP  !number of elements,nodes    
 INTEGER             :: CHUNK,CHUNK_NP,LEFTOVER 
-INTEGER,ALLOCATABLE :: NNELG(:,:) !element connectivity 
+INTEGER,ALLOCATABLE :: NNEL(:,:) !element connectivity 
 INTEGER,ALLOCATABLE :: ITVECT1(:)!vector of node numbers
 INTEGER,ALLOCATABLE :: DMY_UQNNUM(:),UQNNUM(:) !unique node numbers contained in elements
-REAL(8),ALLOCATABLE :: X(:),Y(:) !position     
+REAL(8),ALLOCATABLE :: X(:),Y(:),X_G(:),Y_G(:) !position     
 CONTAINS 
 !---------------------------------------------------------------------
 !      S U B R O U T I N E   R E A D _ G R A P H 
@@ -33,85 +33,98 @@ IF(MYPROC.NE.(NPROC-1)) THEN !if not the last processor
     CHUNK = NE/NPROC
 ELSE 
     CHUNK = NE/NPROC
-    LEFTOVER = NP - CHUNK*(NPROC)         
+    LEFTOVER = NE - CHUNK*NPROC         
     CHUNK = CHUNK + LEFTOVER 
 ENDIF
 
-IF(MYPROC.EQ.0) THEN 
+IF(MYPROC.EQ.1) THEN 
+  PRINT *, "USING THIS MANY CORES",NPROC
   PRINT *, "NUMBER OF ELEMENTS:",NE 
   PRINT *, "NUMBER OF NODES:",NP
   PRINT *, "CHUNK SIZE",CHUNK
+  PRINT *, "LEFTOVER",leftover
 ENDIF     
-
- ALLOCATE(NNELG(3,CHUNK),ITVECT1(3*CHUNK),DMY_UQNNUM(6*CHUNK))
- DMY_UQNNUM(:)=0
+! I choose to allocate 6*CHUNK for DMY_UQNNUM but that is arbitrary
+ ALLOCATE(NNEL(3,CHUNK),ITVECT1(3*CHUNK),DMY_UQNNUM(6*CHUNK))
  ! skip nodes the first time, we'll come back
     DO I = 1,NP 
         READ(13,*)
     ENDDO
-    !
     !--Read Element Connectivity Table in chunks
-    !
+   NNEL=0   
+   IF(MYPROC.NE.(NPROC-1)) THEN 
+    K=1
     DO I = 1,NE
-     IF(I.GE.(MYPROC*CHUNK+1).AND.I.LE.(MYPROC*CHUNK)+CHUNK) THEN 
-       READ(13,*) J,ITEMP,NNELG(1,I-(MYPROC*CHUNK)),NNELG(2,I-(MYPROC*CHUNK)),NNELG(3,I-(MYPROC*CHUNK))
-     ELSE 
-       READ(13,*)
-     ENDIF
-    ENDDO
-
+      IF(I.GE.(MYPROC*CHUNK+1).AND.I.LE.((MYPROC*CHUNK)+CHUNK)) THEN 
+        READ(13,*) J,ITEMP,NNEL(1,K),NNEL(2,K),NNEL(3,K)
+        K = K + 1 
+      ELSE 
+        READ(13,*)
+      ENDIF
+     ENDDO
+   ELSE
+   K=1
+     DO I = 1,NE 
+      IF(I.GE.MYPROC*(CHUNK-LEFTOVER)+1) THEN 
+         READ(13,*) J,ITEMP,NNEL(1,K),NNEL(2,K),NNEL(3,K)
+         K = K + 1
+      ELSE 
+         READ(13,*)
+      ENDIF
+     ENDDO
+   ENDIF
+   CLOSE(13)
 !step 1. find the node numbers associated with the elements that are on myproc
 !step 2. sort them in ascending order 
 !step 3. determine only the unique node numbers
-   !reshape NNELG to vector 
-    ITVECT1=RESHAPE(NNELG,(/CHUNK*3/))
+   !reshape NNEL to vector 
+    ITVECT1=0
+    ITVECT1=RESHAPE(NNEL,(/CHUNK*3/))
     CALL SORT(CHUNK*3,ITVECT1)
-    !only keep unique node numbers 
+    DMY_UQNNUM(:)=0
+   !only keep unique node numbers 
     J=1
     DO I = 1,SIZE(ITVECT1)-1
-        IF(I.EQ.1) THEN 
-         DMY_UQNNUM(1)=ITVECT1(1)
-        ENDIF 
-        IF(ITVECT1(I).EQ.ITVECT1(I+1)) THEN
+       IF(ITVECT1(I).EQ.ITVECT1(I+1).AND.ITVECT1(I).NE.0) THEN
           !skip
         ELSE !store
           DMY_UQNNUM(J)=ITVECT1(I)
           J=J+1
         ENDIF
     ENDDO
-    CLOSE(13)
-! chop off the extra zeros in dmy_uqnnum 
-! cycle if zero
-    J=1
-    DO I = 1,SIZE(DMY_UQNNUM) 
-       IF(DMY_UQNNUM(I).EQ.0) CYCLE
-        J=J+1
-    ENDDO
-    CHUNK_NP = J 
-    ALLOCATE(UQNNUM(CHUNK_NP)) !j is now # of non-zero enteries
-    K=1 !only keep non zero enteries 
+! have to handle the last entry 
+    IF(ITVECT1(SIZE(ITVECT1)).NE.ITVECT1(SIZE(ITVECT1)-1).AND.ITVECT1(SIZE(ITVECT1)).NE.0) THEN 
+     DMY_UQNNUM(J)=ITVECT1(SIZE(ITVECT1))
+     CHUNK_NP=J
+    ELSE 
+     CHUNK_NP=CHUNK_NP-1
+    ENDIF
+    ALLOCATE(UQNNUM(CHUNK_NP))
+    UQNNUM=0
+    K=1 !only keep non zero entries 
     DO I = 1,CHUNK_NP 
         UQNNUM(K)=DMY_UQNNUM(I) 
-         K=K+1
+        K=K+1
     ENDDO 
-
 !now read in the node table associated with those elements 
 !rewind the file to the top 
-    OPEN(13,FILE='fort.14',STATUS='OLD')
-    READ(13,*)
-    READ(13,*) 
-
-    ALLOCATE(X(J),Y(J))
-!--Read nodal connectivity
-    DO I = 1,SIZE(UQNNUM)
-      IF(I.NE.UQNNUM(I)) THEN 
-        READ(13,*) 
-      ELSE    
-        READ(13,*) J,X(I),Y(I)
-      ENDIF
-    ENDDO
-
-!DEBUG 
+   ALLOCATE(X(CHUNK_NP),Y(CHUNK_NP))
+   ALLOCATE(X_G(NP),Y_G(NP))
+!
+   OPEN(13,FILE='fort.14',STATUS='OLD')
+   READ(13,*) 
+   READ(13,*) 
+     DO I = 1,NP 
+       READ(13,*) ITEMP,X_G(I),Y_G(I)
+     ENDDO 
+   CLOSE(13)
+   
+   
+   DO I = 1,CHUNK_NP 
+     X(I)=X_G(UQNNUM(I))
+     Y(I)=Y_G(UQNNUM(I))
+     !PRINT *,I 
+   ENDDO 
 !have each processor write its local grid so we can check it's right
      WRITE(FILENAME,'(A,I3,A)') 'MYPROC_',MYPROC,'.14'
      OPEN(UNIT=MYPROC+105,FILE=FILENAME)
@@ -120,7 +133,7 @@ ENDIF
        WRITE(MYPROC+105,95) I,X(I),Y(I)
      ENDDO 
      DO I = 1,CHUNK 
-       WRITE(MYPROC+105,90) I,3,NNELG(1,I),NNELG(2,I),NNELG(3,I)
+       WRITE(MYPROC+105,90) I,3,NNEL(1,I),NNEL(2,I),NNEL(3,I)
      ENDDO
 100    FORMAT(2I10)
 95     FORMAT(1I10,f10.5,f10.5)
